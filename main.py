2b89ddeacbca02a9c11781a60152fd51dd9150f1
samwel <samwelkaranja890@gmail.comm>
2025-11-02 20:59:43 +0300

    Fix: use Render-assigned PORT

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from weather_api import get_weather
 from outfit_logic import ai_outfit_suggestion
-from notifier import notify  # use the sync wrapper
+from notifier import notify  
 from config import TIME, CITY
 import schedule, time
 from datetime import datetime
 import telebot
 from config import TELEGRAM_TOKEN
+from flask import Flask
+import threading
+from notifier import run_bot
+import os
 
 bot = telebot.TeleBot(TELEGRAM_TOKEN)
 
@@ -30,3 +34,20 @@ print("✅ AI Weather & Outfit Notifier started...")
 while True:
     schedule.run_pending()
     time.sleep(60)
+
+
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return " Weather Outfit Notifier is running!"
+
+def start_bot():
+    run_bot()  
+
+if __name__ == "__main__":
+    threading.Thread(target=start_bot).start()
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
